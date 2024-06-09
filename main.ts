cc311cb6593442447b350eb98b2fe7ecf5d892cb
Iqra <iqraanzish48@gmail.com>
2024-06-09 14:07:57 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/evo node
+ 
+ import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 6);
+
+
+
+const answer = await inquirer.prompt([
+{
+    name:"userGuessedNumber",
+    type:"number",
+    message:"please guess a number between 1-6: ",
+},
+
+]);
+
+if(answer.userGuessedNumber === randomNumber ){
+console.log("Congratulation! you guessed a right  number.")
+}else{
+    console.log("you guessed wrong number")
+}
+
+
+
